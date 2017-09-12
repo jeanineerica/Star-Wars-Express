7aceac464f109ca7570ae39fea99033027e26292
jeanineerica <jeanineerica@me.com>
2017-09-11 21:11:53 -0700

    changed PORT

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var port = process.env.PORT || 3000;
+var port = process.env.PORT;
+//  || 3000;
 
 // Sets up the Express App
 // =============================================================
